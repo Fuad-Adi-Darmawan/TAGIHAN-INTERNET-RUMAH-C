9cf3d72241c926f97bba5b1bcaa9b36a2212b5b4
FAD_ADS <fuadadi137@gmail.com>
2026-07-19 00:11:31 +0700

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("~~~~~TAGIHAN INTERNET RUMAH~~~~~");
+
+    printf("\n");
+
+    char a [20];
+    // printf("Hello World");
+    printf("Nama :");
+    scanf("%s", &a);
+    printf("\nNama: %s", &a);
+
+    printf("\n");
+
+    char b [20];
+    // printf("Hello World");
+    printf("Tagihan_Wifi :");
+    scanf("%s", &a);
+    printf("\nWifi: %s", &a);
+
+    printf("\n");
+
+    char c [20];
+    // printf("Hello World");
+    printf("Tagihan_listrik :");
+    scanf("%s", &a);
+    printf("\nListrik: %s", &a);
+
+    printf("\n");
+
+    char d [20];
+    // printf("Hello World");
+    printf("Total_Tagihan :");
+    scanf("%s", &a);
+    printf("\nTotal_Tagihan: %s", &a);
+
+    printf("\n");
+}
